a64b600fbeef4e69b98d89669c5a4bed6fb63739
gilded1 <31520030+gilded1@users.noreply.github.com>
2021-03-16 17:57:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const express = require(`express`)
 const path = require(`path`)
 const app = express();
 
-<<<<<<< HEAD
+
 app.use(express.static(__dirname + '/dist/'))
 app.use('/src/assets', express.static(__dirname + '/src/assets/'))
 app.listen(process.env.PORT || 8080)
-=======
+
 
 
 client.on(`ready`, () =>{
